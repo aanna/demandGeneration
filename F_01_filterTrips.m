c62a657204bc6fea9642d8296926ab9ac2219ebb
Kasia <kam.washere@gmail.com>
2016-03-09 14:36:01 +0800

    description of the script added, booking_id added, person_id removed, sort function explained

--- F_01_filterTrips.m
+++ F_01_filterTrips.m
@@ -1,9 +1,34 @@
 close all; clear; clc;
 
+% last update: 2016-Mar-09
 % to filter the trips which are within the zone or coming to the zone or
 % ending within the zone
 
 % input is the activity-based schedule from SimMobility mid-term
+% format: (column headers)
+% 1   person_id character varying - unique id for each person
+% 2   tour_no integer - describes if it is 1 or 2 or 3?tour in the day
+% 3   tour_type character varying - purpose of the trip (in therms of the Primary Activity): work/shop/education/other
+% 4   stop_no integer - stop to perform activities
+% 5   stop_type character varying - what is the current activity the person performs
+% 6   stop_location integer, // node id of the trip destination
+% 7   stop_zone integer, // TAZ zone
+% 8   stop_mode character varying, //trip mode
+% 9   primary_stop boolean,
+% 10   arrival_time numeric, // START TIME OF THE ACTIVITY
+% 11  departure_time numeric, // END TIME OF THE ACTIVITY
+% 12  prev_stop_location integer, //trip origin node
+% 13  prev_stop_zone integer,
+% 14  prev_stop_departure_time numeric, //trip start time
+
+% output: 
+% trips in format: 
+% 1 time_sec, integer
+% 2 booking_id, integer
+% 3 trip_origin_node, integer
+% 4 trip_destination_node, integer
+% 5 trip_purpose, character varying
+% 6 trip_mode, character varying
 
 %% import trips for the entire Singapore
 disp('1. Import the activity-based trips...')
@@ -75,6 +100,12 @@ for i = 1: length(person_id)
 end
 
 time_before_convert = trip_start_time(ids_witin);
+% person_id = person_id(ids_witin); do not have to carry it forward
+trip_origin_node = trip_origin_node(ids_witin);
+trip_destination_node = trip_destination_node(ids_witin);
+trip_purpose = trip_purpose(ids_witin);
+trip_mode = trip_mode(ids_witin);
+
 %% convert time to seconds
 % input time is as follows:
 % day starts at 3am and finishes at 26hr
@@ -109,19 +140,24 @@ for i = 1 : length(minutes)
     end
     
 end
-time_sec = hours*60*60 + minutes*60;
+time_sec = hours*60*60 + minutes*60; % size after selection
 
-% sort trips in ascending order of time
-% trips = [time_sec; trip_start_time; person_id; trip_origin_node; trip_destination_node; trip_purpose; trip_mode];
-% trips_sorted = sortrows(trips, 1);
+%% sort trips in ascending order of time
+% to sort the output file use the unix command:
+% i.e., sorting file on the basis of 2nd field , numerically
+% sort -t"," -k2n,2 file 
+% in this specific output file use sort -t"," -k1n,1 file
+% '-t' option is used to provide the delimiter in case of files with 
+% delimiter. '-k' is used to specify the keys on the basis of which 
+% the sorting has to be done. The format of '-k' is : '-km,n' where m is 
+% the starting key and n is the ending key.
 
 %% save to file
 disp('5. Save to file...')
 fileTOSave = sprintf('bookings_ecbd_%d.txt', length(ids_witin));
 bookingFile = fopen(fileTOSave,'w');
 for j = 1:length(ids_witin)
-    id = ids_witin(j);
-    fprintf(bookingFile,'%0u %s %0u %0u %s %s\n', time_sec(j), person_id{id}, trip_origin_node(id), trip_destination_node(id), trip_purpose{id}, trip_mode{id});
+    fprintf(bookingFile,'%0u,%u,%0u,%0u,%s,%s\n', time_sec(j), j, trip_origin_node(j), trip_destination_node(j), trip_purpose{j}, trip_mode{j});
 end
 fclose(bookingFile);
 
